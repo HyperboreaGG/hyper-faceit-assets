07291a2c6ad7808a6c3fd5135b029b66233fa659
Michael Khaimov <khaimovmr@gmail.com>
2020-06-09 13:48:41 +0300

    initial commit

---
+++ wshook-init.js
@@ -0,0 +1,16 @@
+wsHook.after = function(messageEvent, url, wsObject) {
+    var bodyStart = messageEvent.data.search('<body>');
+    var bodyEnd = messageEvent.data.search('</body>');
+
+    if (bodyStart > -1 && bodyEnd > -1) {
+        let message = messageEvent.data.slice(bodyStart + 6, bodyEnd);
+        console.log(message);
+    }
+
+    return messageEvent;
+}
+
+wsHook.after = function(messageEvent, url, wsObject) {
+    wsObject.send("Intercepted and sent again")
+    return null;
+}
